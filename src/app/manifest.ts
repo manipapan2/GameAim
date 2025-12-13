2ceeb4e8d4287bd5aa7f7dcc2b2739fc525f32d3
manipapan2 <manipapan2@gmail.com>
2025-12-13 16:02:08 +0330

    Fix: pwa icons

--- src/app/manifest.ts
+++ src/app/manifest.ts
@@ -11,13 +11,13 @@ export default function manifest(): MetadataRoute.Manifest {
     display: "standalone",
     icons: [
     {
-      src: "/icon-192x192.png",
+      src: "/assets/icons/web-app-manifest-192x192.png",
       sizes: "192x192",
       type: "image/png",
       purpose: "maskable"
     },
     {
-      src: "/icon-512x512.png",
+      src: "/assets/icons/web-app-manifest-512x512.png",
       sizes: "512x512",
       type: "image/png",
       // purpose: "maskable"
